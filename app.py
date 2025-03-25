0339aa670c62f8b5289f6a3c4381ccac48b58621
majiro.dev <majiro.dev@gmail.com>
2025-03-25 12:46:26 +0100

    folder

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 
 from flask import Flask, render_template, request, redirect
+import os
+import sys
 
 app = Flask(__name__)
+folder_path = ''
 
 @app.route('/')
 def insertar_formulario():
     return render_template('index.html')
 
-# gets all the files in the folder and adds them to the left column
-@app.route('/open_folder', methods=['POST'])
-def open_folder():
-	import os
-	files = os.listdir('static/files')
-	return render_template('index.html', files=files)
+# return the file paths from the files in the folder
+@app.route('/get_files')
+def get_files():
+	files = []
+	for file in os.listdir(folder_path):
+		if os.path.isfile(os.path.join(folder_path, file)):
+			files.append(file)
+	return files
 
 if __name__ == '__main__':
+	#get the path from the first argument
+	folder_path = sys.argv[1]
 	app.run(debug=True)
